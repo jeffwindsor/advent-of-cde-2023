40e663b9529e31dfa001b8d30e4e3b896cf710f1
Jeff Windsor <jeff.windsor@gmail.com>
2023-11-30 15:48:35 -0800

    Python Env under Nix

---
+++ shell.nix
@@ -0,0 +1,28 @@
+with import <nixpkgs> {};
+let
+  python-advent = python311.withPackages(ps: with ps; [
+      # advent-of-code-data
+      # advent-of-code-ocr
+      colorama
+      numpy
+      parse
+      pytest
+      rich
+    ]);
+    
+in mkShell {
+  packages = [
+    python-advent
+    nushell
+  ];
+  
+  shellHook = ''
+    echo "  █████╗  ██████╗  ██████╗██████╗  ██████╗ ██████╗ ██████╗ "
+    echo " ██╔══██╗██╔═══██╗██╔════╝╚════██╗██╔═████╗╚════██╗╚════██╗"
+    echo " ███████║██║   ██║██║      █████╔╝██║██╔██║ █████╔╝ █████╔╝"
+    echo " ██╔══██║██║   ██║██║     ██╔═══╝ ████╔╝██║██╔═══╝  ╚═══██╗"
+    echo " ██║  ██║╚██████╔╝╚██████╗███████╗╚██████╔╝███████╗██████╔╝"
+    echo " ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚══════╝ ╚═════╝ ╚══════╝╚═════╝ "
+    echo ""
+  '';
+}
