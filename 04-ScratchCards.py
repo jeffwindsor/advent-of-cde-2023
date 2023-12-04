48095eb03e99d51f5f07c8fb230e59babb41b93d
Jeff Windsor <jeff.windsor@gmail.com>
2023-12-03 22:36:55 -0800

    Day 4 part 2

--- 04-ScratchCards.py
+++ 04-ScratchCards.py
@@ -1,11 +1,23 @@
 import parse
 
-def score(winners, numbers):
-    count = len(set(winners).intersection(set(numbers)))
-    return (2 ** (count - 1)) if count > 0 else 0
+def matches(winners, numbers):
+    return len(set(winners).intersection(set(numbers)))
+
+def score(matches):
+    return (2 ** (matches - 1)) if matches > 0 else 0
 
 def answer_part_1(cards):
-    return sum([score(c['winners'], c['numbers']) for c in cards])
+    return sum([score(matches(c['winners'], c['numbers'])) for c in cards])
+
+# assumes that card input is in order
+def answer_part_2(cards):
+    card_counts_by_id = dict([(c['id'], 1) for c in cards])
+    id_matches = [(c['id'], matches(c['winners'], c['numbers'])) for c in cards]
+    for card_id, card_matches in id_matches:
+        for new_card_id in range(card_id + 1, card_id + 1 + card_matches):
+            # add winnings to card counts
+            card_counts_by_id[new_card_id] = card_counts_by_id[new_card_id] + card_counts_by_id[card_id]    
+    return sum([count for id, count in card_counts_by_id.items()])
     
 ##################################################################
 def parse_as_int(s):
@@ -25,6 +37,8 @@ def main():
     input = parse_input('data/04')
     print('Part 1e:', answer_part_1(example1))
     print('Part 1 :', answer_part_1(input))
+    print('Part 2e:', answer_part_2(example1))
+    print('Part 2 :', answer_part_2(input))
 
 	
 if __name__ == "__main__":
