5d957161f7bf5d3e17bb4d0928dc5ba8e8bbdde6
Jeff Windsor <jeff.windsor@gmail.com>
2023-12-05 18:25:47 -0800

    refactor 5 part 1 verbage

--- 05-IfYouGiveSeedFertilizer.py
+++ 05-IfYouGiveSeedFertilizer.py
@@ -1,41 +1,41 @@
 from functools import reduce
 
-def in_range(start, length, value):
-	return (value - start) >= 0 and (value - start) < length 
+def is_seed_in_range(rangeStart, rangeLength, seed):
+	return (seed - rangeStart) >= 0 and (seed - rangeStart) < rangeLength 
 
-def apply_map(value, map):
-	for destinationStart, sourceStart, length in map:
-		if in_range(sourceStart, length, value):
-			return destinationStart + (value - sourceStart)
-	#return input if no value is in range of any map
-	return value
+def map_seed(seed, planting_map):
+	for destinationStart, sourceStart, length in planting_map:
+		if is_seed_in_range(sourceStart, length, seed):
+			return destinationStart + (seed - sourceStart)
+	#return input if no seed is in range of any planting_map
+	return seed
 	
-def seed2location(seed, maps):
-	return reduce(apply_map, maps, seed)
-	
-def answer_part_1(seeds, maps):
-	return min([seed2location(seed, maps) for seed in seeds])
+def answer_part_1(seeds, planting_maps):
+	return min([reduce(map_seed, planting_maps, seed) for seed in seeds])
+
+
+
+
+
+
 
 def split_into_tuples(xs, chunk_size):
 	for i in range(0, len(xs), chunk_size):
 		yield tuple(xs[i:i + chunk_size])
 
 def answer_part_2(seeds, maps):
-	pass
+	return list(split_into_tuples(seeds, 2))
+
 
 ##################################################################
-def test_in_range_within1(): assert in_range(0,5,4) == True
-def test_in_range_within2(): assert in_range(0,5,0) == True
-def test_in_range_after(): assert in_range(0,5,5) == False
-def test_in_range_before(): assert in_range(1,5,0) == False
+def test_is_seed_in_range_within1(): assert is_seed_in_range(0,5,4) == True
+def test_is_seed_in_range_within2(): assert is_seed_in_range(0,5,0) == True
+def test_is_seed_in_range_after(): assert is_seed_in_range(0,5,5) == False
+def test_is_seed_in_range_before(): assert is_seed_in_range(1,5,0) == False
 
-def test_app_map_example():
-	assert apply_map(79,[(52, 50, 48)]) == 81
+def test_map_seed():
+	assert map_seed(79,[(52, 50, 48)]) == 81
 	
-def test_seed2location_given_no_matches_then_original_input():
-	maps = [[(1,0,5)],[(10,0,5)]]
-	assert seed2location(7, maps) == 7
-
 def test_split_into_tuples_seeds_as_pairs():
 	seeds = [79, 14, 55, 13]
 	assert list(split_into_tuples(seeds, 2)) == [(79, 14), (55, 13)]
@@ -63,7 +63,7 @@ def main():
 	input = parse_input('data/05')
 	print('Part 1e:', answer_part_1(*example1))
 	print('Part 1 :', answer_part_1(*input))
-	# print('Part 2e:', answer_part_2(example1))
+	print('Part 2e:', answer_part_2(*example1))
 	# print('Part 2 :', answer_part_2(input))
 
 	
